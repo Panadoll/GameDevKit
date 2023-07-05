17299dcfcfa48de72b1ceaa6af54cb51cbd8fd90
Panadoll <yingbf@gmail.com>
2023-07-05 14:31:28 +0800

    add
    add

--- Assets/com.panadoll.gamedevkit/Editor/Utillty/UI/UISelector.cs
+++ Assets/com.panadoll.gamedevkit/Editor/Utillty/UI/UISelector.cs
@@ -7,104 +7,108 @@ using System.Text;
 using UnityEditor.SceneManagement;
 using System;
 
-[InitializeOnLoad]
-public class UISelector
+namespace Panadoll
 {
-    private static Dictionary<string, RectTransform> rtDic = new Dictionary<string, RectTransform>();
-    private static StringBuilder sb = new StringBuilder();
-    private readonly static string PREFS_KEY = "editor_setting_uiselector";
-    private static bool enable = true;
-    private static bool init = false;
-
-    static UISelector()
+    [InitializeOnLoad]
+    public class UISelector
     {
-        if (init == false)
-        {
-            init = true;
-            enable = PlayerPrefs.GetInt(PREFS_KEY, 0) == 1;
-        }
-        SceneView.duringSceneGui += OnSceneGUI;
-    }
+        private static Dictionary<string, RectTransform> rtDic = new Dictionary<string, RectTransform>();
+        private static StringBuilder sb = new StringBuilder();
+        private readonly static string PREFS_KEY = "editor_setting_uiselector";
+        private static bool enable = true;
+        private static bool init = false;
 
-    private static RectTransform[] GetAllRectTransforms()
-    {
-        PrefabStage prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
-        if (prefabStage != null)
+        static UISelector()
         {
-            return prefabStage.prefabContentsRoot.GetComponentsInChildren<RectTransform>();
-        }
-        else
-        {
-            return GameObject.FindObjectsOfType<RectTransform>();
-        }
-    }
-
-    private static bool GetComponents<T>(RectTransform rt)
-    {
-        var components = rt.GetComponents<T>();
-        if (components != null && components.Length > 0)
-        {
-            sb.Length = 0;
-            sb.AppendFormat("{0}.[{1},{2}", rtDic.Count, typeof(T).Name, rt.name);
-            rtDic.Add(sb.ToString(), rt);
-            return true;
-        }
-        return false;
-    }
-
-    private static void OnSceneGUI(SceneView sceneView)
-    {
-        var ec = Event.current;
-        if (ec != null && ec.button == 1 && ec.type == EventType.MouseUp && enable)
-        {
-            ec.Use();
-            Vector2 mousePos = ec.mousePosition;
-            float ppp = EditorGUIUtility.pixelsPerPoint;
-            mousePos.y = sceneView.camera.pixelHeight - mousePos.y * ppp;
-            mousePos.x *= ppp;
-
-            var rts = GetAllRectTransforms();
-            if (rts == null)
-                return;
-
-            Array.Reverse(rts);
-            rtDic.Clear();
-            foreach (var rt in rts)
+            if (init == false)
             {
-                if (rt.gameObject.activeInHierarchy && RectTransformUtility.RectangleContainsScreenPoint(rt, mousePos, sceneView.camera))
-                {
-                    if (GetComponents<Button>(rt)) continue;
-                    if (GetComponents<ScrollRect>(rt)) continue;
-                    if (GetComponents<Image>(rt)) continue;
-                    if (GetComponents<Text>(rt)) continue;
-                    if (GetComponents<RawImage>(rt)) continue;
-                    //if (GetComponents<RawImage>(rt)) continue;
-                }
+                init = true;
+                enable = PlayerPrefs.GetInt(PREFS_KEY, 0) == 1;
             }
-            if (rtDic.Count > 0)
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
+
+        private static RectTransform[] GetAllRectTransforms()
+        {
+            PrefabStage prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            if (prefabStage != null)
             {
-                var menu = new GenericMenu();
-                foreach (var item in rtDic)
+                return prefabStage.prefabContentsRoot.GetComponentsInChildren<RectTransform>();
+            }
+            else
+            {
+                return GameObject.FindObjectsOfType<RectTransform>();
+            }
+        }
+
+        private static bool GetComponents<T>(RectTransform rt)
+        {
+            var components = rt.GetComponents<T>();
+            if (components != null && components.Length > 0)
+            {
+                sb.Length = 0;
+                sb.AppendFormat("{0}.[{1},{2}", rtDic.Count, typeof(T).Name, rt.name);
+                rtDic.Add(sb.ToString(), rt);
+                return true;
+            }
+            return false;
+        }
+
+        private static void OnSceneGUI(SceneView sceneView)
+        {
+            var ec = Event.current;
+            if (ec != null && ec.button == 1 && ec.type == EventType.MouseUp && enable)
+            {
+                ec.Use();
+                Vector2 mousePos = ec.mousePosition;
+                float ppp = EditorGUIUtility.pixelsPerPoint;
+                mousePos.y = sceneView.camera.pixelHeight - mousePos.y * ppp;
+                mousePos.x *= ppp;
+
+                var rts = GetAllRectTransforms();
+                if (rts == null)
+                    return;
+
+                Array.Reverse(rts);
+                rtDic.Clear();
+                foreach (var rt in rts)
                 {
-                    menu.AddItem(new GUIContent(item.Key), false, () =>
+                    if (rt.gameObject.activeInHierarchy && RectTransformUtility.RectangleContainsScreenPoint(rt, mousePos, sceneView.camera))
                     {
-                        Selection.activeTransform = item.Value;
-                        EditorGUIUtility.PingObject(item.Value.gameObject);
-                    });
+                        if (GetComponents<Button>(rt)) continue;
+                        if (GetComponents<ScrollRect>(rt)) continue;
+                        if (GetComponents<Image>(rt)) continue;
+                        if (GetComponents<Text>(rt)) continue;
+                        if (GetComponents<RawImage>(rt)) continue;
+                        //if (GetComponents<RawImage>(rt)) continue;
+                    }
                 }
-                menu.ShowAsContext();
+                if (rtDic.Count > 0)
+                {
+                    var menu = new GenericMenu();
+                    foreach (var item in rtDic)
+                    {
+                        menu.AddItem(new GUIContent(item.Key), false, () =>
+                        {
+                            Selection.activeTransform = item.Value;
+                            EditorGUIUtility.PingObject(item.Value.gameObject);
+                        });
+                    }
+                    menu.ShowAsContext();
+                }
             }
-        }
 
-        Handles.BeginGUI();
-        GUILayout.BeginArea(new Rect(10, sceneView.camera.pixelHeight - 30, 100, 30));
-        EditorGUI.BeginChangeCheck();
-        enable = GUILayout.Toggle(enable, "UISelector");
-        if (EditorGUI.EndChangeCheck())
-        {
-            PlayerPrefs.SetInt(PREFS_KEY, enable ? 1 : 0);
+            Handles.BeginGUI();
+            GUILayout.BeginArea(new Rect(10, sceneView.camera.pixelHeight - 30, 100, 30));
+            EditorGUI.BeginChangeCheck();
+            enable = GUILayout.Toggle(enable, "UISelector");
+            if (EditorGUI.EndChangeCheck())
+            {
+                PlayerPrefs.SetInt(PREFS_KEY, enable ? 1 : 0);
+            }
+            GUILayout.EndArea();
+            Handles.EndGUI();
         }
-        GUILayout.EndArea();
-        Handles.EndGUI();
     }
 }
+
